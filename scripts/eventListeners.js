ea34cdf0d0f3e5f195294a900ca6196afb0a126f
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-05-23 12:24:53 +0200

    started save/load

--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -16,6 +16,14 @@ function keyPressed() {
   if (keyCode === 32) {
     isPlaying = !isPlaying;
   }
+
+  if (keyCode === 83) {
+    //save
+  }
+
+  if (keyCode === 76) {
+    //load
+  }
 }
 
 function togglePlaying() {
